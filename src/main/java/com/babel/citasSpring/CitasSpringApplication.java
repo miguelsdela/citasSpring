69c032735a2eecf1eb4cd024bfad91309e25f103
miguel.sdela <miguel.sdela@babelgroup.com>
2023-03-06 11:29:22 +0100

    Primer commit

---
+++ src/main/java/com/babel/citasSpring/CitasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.babel.citasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitasSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babel/citasSpring/CitasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.citasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
