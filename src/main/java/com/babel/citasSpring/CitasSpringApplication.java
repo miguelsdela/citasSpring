dc0a6dd2867ebd61b1742e8ea1f97a2845aeb2a7
miguel.sdela <miguel.sdela@babelgroup.com>
2023-03-07 14:19:06 +0100

    Maneras de no mostrar el Banner de Spring

--- src/main/java/com/babel/citasSpring/CitasSpringApplication.java
+++ src/main/java/com/babel/citasSpring/CitasSpringApplication.java
@@ -1,5 +1,6 @@
 package com.babel.citasSpring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitasSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CitasSpringApplication.class, args);
+        //SpringApplication.run(CitasSpringApplication.class, args);
+        //Para no mostrar el Banner de Spring se puede hacer así
+        SpringApplication app = new SpringApplication(CitasSpringApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
         //ConsoleReader consoleReader = new ConsoleReader(new CitasServiceImpl(new CitaRepositoryMemory()));
     }
 
